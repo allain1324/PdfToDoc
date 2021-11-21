49116c945d20d3c929c1810cd4dbe3f4ba4d01ef
mtoan65 <toandinh6501@outlook.com>
2021-11-21 15:58:55 +0700

    upload file

--- src/main/java/controller/CheckLoginServlet.java
+++ src/main/java/controller/CheckLoginServlet.java
@@ -1,7 +1,7 @@
 package controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
--- src/main/java/model/dao/checkLoginDAO.java
+++ src/main/java/model/dao/checkLoginDAO.java
@@ -1,24 +1,26 @@
 package model.dao;
-
+//import java.sql.Connection;
 import java.sql.DriverManager;
+//import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+//import java.sql.Statement;
 
 import model.bean.account;
 
 public class checkLoginDAO {
-	public static account getAccountDAO(String username, String password) {
+	public static account getAccountDAO(String username, String password){
 		try {
 			 java.sql.Connection con;
 			 
 			 String query = "SELECT * FROM account where username= ? AND password = ?";
-			 Class.forName("com.mysql.cj.jdbc.Driver");
+//			 Class.forName("com.mysql.cj.jdbc.Driver");
+			 Class.forName("com.mysql.jdbc.Driver");
 			 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_LTMang","root","");
 			 PreparedStatement prepare = con.prepareStatement(query);
 			 prepare.setString(1,username);
 			 prepare.setString(2,password);
-			 Statement sqlStatement = con.createStatement();
+			 //Statement sqlStatement = con.createStatement();
 			 ResultSet rs = prepare.executeQuery();
 		     if(rs.next()){
 		    	 int idAccount = Integer.parseInt(rs.getString(1));
